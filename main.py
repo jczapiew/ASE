6da9bce7d164cd9b4a13d0d2a1baadf1a1c104ff
pawellogin7 <pawellogin7@gmail.com>
2020-09-16 17:16:20 +0200

    Przetwarzanie plików WARC, zliczanie słów i zapisanie wyniku do pliku .csv na buckecie

--- main.py
+++ main.py
@@ -1,75 +1,54 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession, SQLContext
 import boto3
-import html2text
 from warcio.archiveiterator import ArchiveIterator
 from warcio.bufferedreaders import BytesIO
 from bs4 import BeautifulSoup
 import datetime
+import re
 
-interesting_words = ["wirus", "pandemia", "covid", "covid19", "biznes", "polska"]
-word_count = [0] * len(interesting_words)
 
 # ==============================================================
 # ==================== Deklaracje funkcji ======================
 # ==============================================================
 
-# Funkcja zamieniająca rekord zmiennej dataframe na wpis w zmiennej dictionary
-def process_warc_path_df(record):
-    return {'url': record.url, 'warc_filename': record.warc_filename,
-            'warc_record_offset': record.warc_record_offset, 'warc_record_length': record.warc_record_length}
+# Funkcja wczytująca i przetwarzająca pliki WARC z Common Crawl w celu zliczenia wystąpień słów kluczy
+# w danym przedziale czasu.
+# Funkcja zwraca klucz w postaci data + słowo klucz oraz ilość wystąpień danego słowa klucza
+def process_warc_records(warc_records):
+    s3_client = boto3.client('s3')
 
+    for record in warc_records:
+        warc_path = record['warc_filename']
+        warc_offset = int(record['warc_record_offset'])
+        warc_length = int(record['warc_record_length'])
+        warc_range = 'bytes={}-{}'.format(warc_offset, (warc_offset+warc_length-1))
+        # Wczytywanie pliku WARC z Common Crawl
+        response = s3_client.get_object(Bucket='commoncrawl', Key=warc_path, Range=warc_range)
 
-# Funkcja odczytująca i przetwarzająca pliki WARC z Common Crawl(wersja testowa, do zmiany)
-def fetch_process_warc_records(rows):
-    global interesting_words, word_count
-    print('>>>>>>>>>>>>>>>>>')
-    s3client = boto3.client('s3')
+        # Odczytywanie danych z pliku WARC
+        warc_record_stream = BytesIO(response["Body"].read())
 
-    for row in rows:
-        url = row['url']
-        warc_path = row['warc_filename']
-        offset = int(row['warc_record_offset'])
-        length = int(row['warc_record_length'])
-        rangereq = 'bytes={}-{}'.format(offset, (offset+length-1))
-        response = s3client.get_object(Bucket='commoncrawl', Key=warc_path, Range=rangereq)
-
-        record_stream = BytesIO(response["Body"].read())
-
-        parser = html2text.HTML2Text()
-        parser.ignore_links = True
-        for record in ArchiveIterator(record_stream):
-            date_str = record.rec_headers.get_header("WARC-Date").split("T")[0]
+        # Przetwarzanie rekordów pliku WARC
+        for warc_record in ArchiveIterator(warc_record_stream):
+            # Odczytywanie daty pliku WARC
+            date_str = warc_record.rec_headers.get_header("WARC-Date").split("T")[0]
             date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
-            print('Date: ', date_obj)
-            page = record.content_stream().read()
-
+            # Czytanie danych zapisanych w postaci HTML
+            page = warc_record.content_stream().read()
             soup = BeautifulSoup(page, features="html.parser")
-
             # wyrzucenie elementów 'script' i 'style'
             for script in soup(["script", "style"]):
                 script.extract()
-
             # zamiana na czysty tekst
-            text = soup.get_text()
-            # linijka po linijce bez białych znaków na końcach
-            lines = (line.strip() for line in text.splitlines())
-            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-            # wyrzucenie pustych linii
-            text = '\n'.join(chunk for chunk in chunks if chunk)
+            page_text = soup.get_text().lower()
 
-            # liczenie słów
-            for word in text.split():
-                if len(word) < 4:  # odrzucanie krotkich slow (np. "na")
-                    continue
+            # Zliczanie wystąpień każdego słowa klucza w tekście
+            for word in word_patterns:
+                count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(word), page_text))
+                key = str(date_obj) + '/' + word  # Klucz = data pliku WARC + słowo klucz
+                yield key, count
 
-                word = word.lower()
-                # jeśli słowo jest tym którego szukamy to ++
-                if word in interesting_words:
-                    i = interesting_words.index(word)
-                    word_count[i] += 1
-
-    print('>>>>>>>>>>>>>>>>>')
 
 
 # ==============================================================
@@ -79,6 +58,9 @@ def fetch_process_warc_records(rows):
 print("==============================================")
 print("+--------------------------------------------+")
 print("==============================================")
+
+# Słowa klucze
+word_patterns = ['covid', 'covid19', 'pandemia', 'wirus']
 
 # Inicializacja Sparka
 conf = (SparkConf()
@@ -94,20 +76,62 @@ input_bucket = 's3://commoncrawl/cc-index/table/cc-main/warc/'
 df = sql_context.read.load(input_bucket)
 df.createOrReplaceTempView('ccindex')
 
-# Wybranie przedziału danych, które nas interesują, za pomocą odpowiedniego zapytania
-query_txt = 'SELECT url, warc_filename, warc_record_offset, warc_record_length FROM ccindex ' \
-            'WHERE crawl = "CC-MAIN-2020-16" AND subset = "warc" AND url_host_tld = "pl"'
-sqlDF = sql_context.sql(query_txt).limit(100)
+# Użycie odpowiedniego zapytania SQL w celu stworzenia DataFrame, który zawiera spis plików WARC
+# z interesującego nas przedziału danych
+query_txt = 'SELECT warc_filename, warc_record_offset, warc_record_length FROM ccindex ' \
+            'WHERE (crawl = "CC-MAIN-2019-13" OR crawl = "CC-MAIN-2019-18" OR crawl = "CC-MAIN-2020-16") ' \
+            'AND subset = "warc" AND url_host_tld = "pl"'
+sqlDF = sql_context.sql(query_txt)
 # sqlDF.show()
 
-# Zamiana zmiennej typu dataframe, zawierającej linki do plików WARC, na zmienną typu dictionary
-warc_paths_rdd = sqlDF.rdd.map(process_warc_path_df).collect()
+# Odczytanie i przetwarzanie plików WARC(zliuczanie wystąpień słów kluczy w danym dniu)
+word_counts = sqlDF.rdd.mapPartitions(process_warc_records).reduceByKey(lambda a, b: a + b).collect()
 
-# Odczytanie i przetwarzanie plików WARC
-fetch_process_warc_records(warc_paths_rdd)
+# Sparsowanie wyników
+word_counts_array = []
+for row in word_counts:
+    word_key = row[0].split('/')
+    date = word_key[0]
+    word = word_key[1]
+    count = int(row[1])
+    word_counts_array.append([date, word, count])
+# Sortowanie wyników względem daty
+word_counts_sorted = sorted(word_counts_array, key=lambda word_row: word_row[0])
 
-print(interesting_words)
-print(word_count)
+# Stworzenie finalnej tablicy z danymi w celu łatwiejszej ich wizualizacji i przetwarzania(rządy to daty, kolumny to słowa klucze)
+word_counts_final = []
+index_row = ['DATE']
+for word in word_patterns:
+    index_row.append(word)
+
+word_counts_final.append(index_row)
+temp_row = [word_counts_sorted[0][0]]
+for i in range(len(word_patterns)):
+    temp_row.append(0)
+word_counts_final.append(temp_row)
+
+for row in word_counts_sorted:
+    items = len(word_counts_final)
+    date = row[0]
+    word = row[1]
+    count = row[2]
+    if word_counts_final[items - 1][0] != date:
+        new_row = [date]
+        for i in range(len(word_patterns)):
+            new_row.append(0)
+        word_counts_final.append(new_row)
+        items = len(word_counts_final)
+    for i in range(1, len(index_row), 1):
+        key_word = index_row[i]
+        if word == key_word:
+            word_counts_final[items - 1][i] = count
+            break
+
+# Zamiana finalnej tabeli na DataFrame i zapisanie danych do pliku .csv
+df_words = sc.parallelize(word_counts_final).toDF(index_row)
+df_words.repartition(1).write.csv('s3://aseeee/dane.csv')
+df_words.show()
+
 
 print("==============================================")
 print("+--------------------------------------------+")
