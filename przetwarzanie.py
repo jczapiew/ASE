6fea4a0d32928b3c821fcd3b96472c441dc94142
Marcin Wankiewicz <marcin.wankiewicz97@gmail.com>
2020-09-18 08:22:34 +0200

    skrypt wykresowy i wykres

--- przetwarzanie.py
+++ przetwarzanie.py
@@ -1,56 +1,56 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-df = pd.read_csv("dane_ase.csv")
-print(df)
+fig, axs = plt.subplots(3, figsize=(7, 7))
+fig.tight_layout(pad=4)
+fig.suptitle('Wystąpienia słów związanych z pandemią')
+axs[0].set(xlabel='data', ylabel='liczba wystąpień')
+axs[1].set(xlabel='data', ylabel='liczba wystąpień')
+axs[2].set(xlabel='data', ylabel='liczba wystąpień')
 
+
+df = pd.read_csv("dane_2019_03.csv")
 dates = df.iloc[:, 0].to_list()
 covids = df['covid'].to_list()
 covid19s = df['covid19'].to_list()
 pandemias = df['pandemia'].to_list()
 wiruses = df['wirus'].to_list()
-bos = df['bo'].to_list()
-ass = df['a'].to_list()
 
-index = 0
-for i, date in enumerate(dates):
-    if "2020" in date:
-        index = i
-        break
-print(index)
-#
-# for date in dates:
-#     date = date + " 00:00:00"
-#     print (date)
+axs[0].plot(dates, covids, label='covid')
+axs[0].plot(dates, covid19s, label='covid19')
+axs[0].plot(dates, pandemias, label='pandemia')
+axs[0].plot(dates, wiruses, label='wirus')
 
 
-fig, axs = plt.subplots(2)
-fig.tight_layout(pad=4)
-fig.suptitle('Wystąpienia słów związanych z pandemią')
-axs[0].set(xlabel='data', ylabel='liczba wystąpień')
-axs[1].set(xlabel='data', ylabel='liczba wystąpień')
-axs[0].plot(dates[0:index-1], bos[0:index-1], label='bo')
-axs[0].plot(dates[0:index-1], ass[0:index-1], label='bo')
-axs[1].plot(dates[index:len(dates)-1], bos[index:len(dates)-1], label='bo')
-axs[1].plot(dates[index:len(dates)-1], ass[index:len(dates)-1], label='a')
+df = pd.read_csv("dane_2019_04.csv")
+dates = df.iloc[:, 0].to_list()
+covids = df['covid'].to_list()
+covid19s = df['covid19'].to_list()
+pandemias = df['pandemia'].to_list()
+wiruses = df['wirus'].to_list()
+
+axs[1].plot(dates, covids, label='covid')
+axs[1].plot(dates, covid19s, label='covid19')
+axs[1].plot(dates, pandemias, label='pandemia')
+axs[1].plot(dates, wiruses, label='wirus')
+
+
+df = pd.read_csv("dane_2020_2pr.csv")
+dates = df.iloc[:, 0].to_list()
+covids = df['covid'].to_list()
+covid19s = df['covid19'].to_list()
+pandemias = df['pandemia'].to_list()
+wiruses = df['wirus'].to_list()
+
+axs[2].plot(dates, covids, label='covid')
+axs[2].plot(dates, covid19s, label='covid19')
+axs[2].plot(dates, pandemias, label='pandemia')
+axs[2].plot(dates, wiruses, label='wirus')
+
 axs[0].tick_params(labelrotation=30)
 axs[1].tick_params(labelrotation=30)
+axs[2].tick_params(labelrotation=30)
 axs[0].legend()
 axs[1].legend()
-# plt.xticks(rotation=30)
-# fig.autofmt_xdate()
-# axs[0].xaxis_date()
-# fig.autofmt_xdate()
+axs[2].legend()
 plt.show()
-
-# plt.title("2019")
-# plt.plot(dates[0:index-1], bos[0:index-1], label='bo')
-# plt.plot(dates[0:index-1], bos[0:index-1], label='bo')
-# plt.legend()
-# plt.show()
-#
-# plt.title("2020")
-# plt.plot(dates[index:len(dates)-1], bos[index:len(dates)-1], label='bo')
-# plt.plot(dates[index:len(dates)-1], ass[index:len(dates)-1], label='a')
-# plt.legend()
-# plt.show()
